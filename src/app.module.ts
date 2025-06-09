1c561342a5336699aaf698f7f1fbe9d16bc96fba
PratikPatil3235 <pratikpatil@medianv.com>
2025-06-09 09:49:49 +0530

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+import { CommentModule } from './comment/comment.module';
+
+@Module({
+  imports: [UserModule, PostModule, CommentModule],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
